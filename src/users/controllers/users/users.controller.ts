52a6829640f9c9c22d757a7195d5cec057727ad8
husamahmud <husamahmud@gmail.com>
2024-08-31 21:16:23 +0300

    feat: RouteParams

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,6 +1,5 @@
-import { Request, Response } from 'express';
-
-import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateUserDto } from '../../../common/dto/CreateUser.dto';
 
 @Controller('users')
 export class UsersController {
@@ -23,9 +22,17 @@ export class UsersController {
     ];
   }
 
-  @Post()
-  createUser(@Req() request: Request, @Res() response: Response) {
-    console.log(request.body);
-    response.send('User created');
+  //! Route params
+  @Get(':id/:name')
+  getUserById(@Param('id') id: string, @Param('name') name: string) {
+    console.log('id', id);
+    console.log('name', name);
+    return { id, name };
+  }
+
+  @Post('/create')
+  createUserPost(@Body() userData: CreateUserDto) {
+    console.log(userData);
+    return {};
   }
 }
